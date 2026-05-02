3d780e02386ba719616c2b40151f6783fca5ced3
osadaRajapaksha <osadanirmal9@gmail.com>
2026-05-02 05:49:04 +0530

    components were added using shadecn

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), //aliases the bundler to the src folder, so we can import files from the src folder using @ instead of relative paths. For example, instead of importing a file like this: import MyComponent from '../../components/MyComponent', we can import it like this: import MyComponent from '@/components/MyComponent'. This makes our imports cleaner and easier to read.
+    },
+  },
 })
